5996b3fbcaa09164a4f10c65da7b987a7c71fde4
serwennn <nassen.d230@gmail.com>
2024-07-31 13:26:26 +0500

    another example code from https://github.com/bwmarrin/discordgo

--- cmd/drew4/main.go
+++ cmd/drew4/main.go
@@ -3,119 +3,71 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/bwmarrin/discordgo"
-	"log"
 	"os"
 	"os/signal"
-	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 )
 
-type optionMap = map[string]*discordgo.ApplicationCommandInteractionDataOption
-
-func parseOptions(options []*discordgo.ApplicationCommandInteractionDataOption) (om optionMap) {
-	om = make(optionMap)
-	for _, opt := range options {
-		om[opt.Name] = opt
-	}
-
-	return
-}
-
-func interactionAuthor(i *discordgo.Interaction) *discordgo.User {
-	if i.Member != nil {
-		return i.Member.User
-	}
-
-	return i.User
-}
-
-func handleEcho(s *discordgo.Session, i *discordgo.InteractionCreate, opts optionMap) {
-	builder := new(strings.Builder)
-	if v, ok := opts["author"]; ok && v.BoolValue() {
-		author := interactionAuthor(i.Interaction)
-		builder.WriteString(author.String() + " bro really said ")
-	}
-	builder.WriteString(opts["message"].StringValue())
-
-	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseChannelMessageWithSource,
-		Data: &discordgo.InteractionResponseData{
-			Content: builder.String(),
-		},
-	})
-	if err != nil {
-		log.Panicf("Failed to respond to interaction: %s", err)
-	}
-}
-
-var commands = []*discordgo.ApplicationCommand{
-	{
-		Name:        "crash",
-		Description: "Crash the server",
-		Options: []*discordgo.ApplicationCommandOption{
-			{
-				Name:        "message",
-				Description: "Say your last words",
-				Type:        discordgo.ApplicationCommandOptionString,
-				Required:    true,
-			},
-			{
-				Name:        "author",
-				Description: "Whether to prepend message's author",
-				Type:        discordgo.ApplicationCommandOptionBoolean,
-			},
-		},
-	},
-}
-
+// Variables used for command line parameters
 var (
-	Token = flag.String("token", "", "Discord bot token")
-	App   = flag.String("app", "", "Application ID")
-	Guild = flag.String("guild", "", "Guild ID")
+	Token string
 )
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
 
 func main() {
-	fmt.Printf("%s\n", *Token)
-	flag.Parse()
-	if *App == "" {
-		log.Fatal("Application ID is required")
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
 	}
 
-	session, _ := discordgo.New("Bot " + *Token)
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
 
-	session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		if i.Type != discordgo.InteractionApplicationCommand {
-			return
-		}
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
 
-		data := i.ApplicationCommandData()
-		if data.Name != "crash" {
-			return
-		}
-
-		handleEcho(s, i, parseOptions(data.Options))
-	})
-
-	session.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
-		log.Printf("Logged in as %s", r.User.String())
-	})
-
-	_, err := session.ApplicationCommandBulkOverwrite(*App, *Guild, commands)
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
 	if err != nil {
-		log.Fatalf("Failed to register commands: %s", err)
+		fmt.Println("error opening connection,", err)
+		return
 	}
 
-	err = session.Open()
-	if err != nil {
-		log.Fatalf("Failed to open session: %s", err)
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
 	}
 
-	sigch := make(chan os.Signal, 1)
-	signal.Notify(sigch, os.Interrupt)
-	<-sigch
-
-	err = session.Close()
-	if err != nil {
-		log.Fatalf("Failed to close session gracefully: %s", err)
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
 	}
 }
